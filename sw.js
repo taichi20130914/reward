b9e21284f082da0b855c2159d3b87e2f636f3461
taichi20130914 <mineojan.jr@gmail.com>
2026-09-20 13:15:01 +0900

    Add service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "reworld-v1";
+
+const FILES_TO_CACHE = [
+  "/reward/",
+  "/reward/index.html",
+  "/reward/manifest.json",
+  "/reward/icon-192.png",
+  "/reward/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
